2fbf0f7b59bcba1a00e41cc2b1679a58f360157c
barrosol <lucas.barroso@impactzero.pt>
2022-11-08 17:35:46 +0000

    meal prep app setut, tabbar, homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <span>
+    <HomeHero></HomeHero>
+  </span>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HomeHero from "@/components/HomeHero.vue";
 
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+  components: { HomeHero },
 };
 </script>
